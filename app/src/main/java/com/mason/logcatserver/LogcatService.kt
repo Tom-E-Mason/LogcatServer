2030d389ac7cdd387dc17ef013340d5190647e56
Tom Mason <tom.mason@vncautomotive.com>
2024-06-29 12:04:24 +0100

    moved server to service.

---
+++ app/src/main/java/com/mason/logcatserver/LogcatService.kt
@@ -0,0 +1,57 @@
+package com.mason.logcatserver
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class LogcatService : Service() {
+
+    private val server = Server()
+    private val advertiser = Advertiser(Build.ID)
+    private val _started = MutableStateFlow(false)
+
+    override fun onStartCommand(
+        intent: Intent?,
+        flags: Int,
+        startId: Int
+    ): Int {
+        super.onStartCommand(intent, flags, startId)
+        Log.i(TAG, "onStartCommand")
+        _started.value = true
+        return START_STICKY
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return LogcatServiceClient()
+    }
+
+    inner class LogcatServiceClient : Binder() {
+
+        val started: StateFlow<Boolean>
+            get() = _started
+
+        fun start() {
+            Log.i(TAG, "start")
+            startService(Intent(this@LogcatService, LogcatService::class.java))
+            server.start()
+            advertiser.start()
+        }
+
+        fun stop() {
+            Log.i(TAG, "stop")
+            server.stop()
+            advertiser.stop()
+            _started.value = false
+            stopService(Intent(this@LogcatService, LogcatService::class.java))
+        }
+    }
+
+    companion object {
+        private const val TAG = "LogcatService"
+    }
+}
--- app/src/main/java/com/mason/logcatserver/MainActivity.kt
+++ app/src/main/java/com/mason/logcatserver/MainActivity.kt
@@ -1,23 +1,49 @@
 package com.mason.logcatserver
 
-import android.os.Build
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.IBinder
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mason.logcatserver.LogcatService.LogcatServiceClient
 import com.mason.logcatserver.ui.theme.LogcatServerTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val server = Server()
-    private val advertiser = Advertiser(Build.ID)
+    private val client = mutableStateOf<LogcatServiceClient?>(null)
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(
+            name: ComponentName?,
+            binder: IBinder?,
+        ) {
+            client.value = binder as LogcatServiceClient
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            client.value = null
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,7 +54,36 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(16.dp),
+                    ) {
+                        val client = client.value
+                        if (client != null) {
+                            val started by client.started.collectAsState()
+                            Button(
+                                onClick = {
+                                    if (started) {
+                                        client.stop()
+                                    } else {
+                                        client.start()
+                                    }
+                                },
+                            ) {
+                                Text(if (started) "Stop" else "Start")
+                            }
+                        } else {
+                            Button(
+                                onClick = {},
+                                enabled = false,
+                            ) {
+                                Text("Service not bound.")
+                            }
+                        }
+                    }
                 }
             }
         }
@@ -37,15 +92,14 @@ class MainActivity : ComponentActivity() {
     override fun onStart() {
         super.onStart()
         Log.i(TAG, "onStart")
-        server.start()
-        advertiser.start()
+        val intent = Intent(this, LogcatService::class.java)
+        bindService(intent, connection, Context.BIND_AUTO_CREATE)
     }
 
     override fun onStop() {
         super.onStop()
         Log.i(TAG, "onStop")
-        advertiser.stop()
-        server.stop()
+        unbindService(connection)
     }
 
     companion object {
